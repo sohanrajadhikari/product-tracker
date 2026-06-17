6bb129d8cb3d6b2825a4b0b01859e6096fdba6b8
sohanrajadhikari <213793771+Sohan-Raj-Adhikari@users.noreply.github.com>
2026-06-17 11:24:06 +0545

    Added bs4 to parse the data.

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright
 from playwright_stealth import Stealth
 import re
+from bs4 import BeautifulSoup
 
 def clean_daraz_url(url: str) -> str:
     match = re.match(
@@ -21,15 +22,27 @@ def track_new():
         print(f"Error : {e}")
     else:
         with Stealth().use_sync(sync_playwright()) as playwright:
-            browser = playwright.chromium.launch(headless=False)
+            browser = playwright.chromium.launch(headless=True)
             context = browser
 
             page = context.new_page()
 
             try:
-                page.goto(url)
-                print(page.title())
+                page.goto(url, wait_until="networkidle")
+
+                html = page.content()
+
+                soup = BeautifulSoup(html, 'html.parser')
+                
+                title = soup.find("h1", class_="pdp-mod-product-badge-title").get_text(strip=True)
+                
+                string_price = soup.find("span", class_="pdp-price_type_normal").get_text(strip=True)
+                match = re.search(r'(\d+)', string_price)
+                price = int(match.group(1)) if match else None
+
+
+                print(title, price)
                 return
-            except:
-                print("Your url was not valid.")
+            except Exception as e:
+                print(f"Error : {e}")
                 return
